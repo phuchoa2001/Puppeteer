9dd0d82be0e57dbb0ca20fdb59d68b91761f4f01
phclone <phclone.fe@gmail.com>
2022-11-12 09:31:06 +0700

    update

---
+++ db/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+async function connect() {
+    var db = null
+    try {
+        await mongoose.connect(process.env.DATA_BASE_URL, { // kết nối MongDB
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useFindAndModify: false,
+            // useCreateIndex: true
+        });
+        console.log("đã kết Mongdb Thành công");
+    } catch (error) {
+        console.log("Kết nối Mongdb Không Thành công");
+    }
+    return db;
+}
+module.exports = { connect }
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   puppeteer = require("puppeteer");
 }
 
-app.get("/api", async (req, res) => {
+app.get("/phuchoa00/All", async (req, res) => {
   let options = {};
 
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
@@ -27,8 +27,75 @@ app.get("/api", async (req, res) => {
     let browser = await puppeteer.launch(options);
 
     let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+    await page.setViewport({
+      width: 1400,
+      height: 1000,
+    });
+    async function GetAll() {
+      await page.goto(`https://dichthuatphuongdong.com/tienich/random-vietnamese-profile.html`);
+      const User = await page.evaluate(() => {
+        const resultArr = {};
+        const Users = document.querySelectorAll('table.table.common-table tbody tr')
+        for (let i = 0; i < Users.length; i++) {
+          const item = Users[i];
+          const ItemArr = item.querySelectorAll("td");
+          const key = ItemArr[0].querySelector("span").innerText.replace(/\s/g, '');
+          const value = ItemArr[1].querySelector("strong").innerText;
+          resultArr[key] = value;
+        }
+        return resultArr;
+      })
+
+      const Info = await page.evaluate(() => {
+        const resultArr = {};
+        const Address = document.querySelectorAll('table.table')[1];
+        const Info = Address.querySelectorAll("tbody tr");
+        for (let i = 1; i < Info.length; i++) {
+          const item = Info[i];
+          const ItemArr = item.querySelectorAll("td");
+          const key = ItemArr[0].innerText.replace(/\s/g, '');
+          const value = ItemArr[1].querySelector("strong").innerText;
+          resultArr[key] = value;
+        }
+        return resultArr;
+      })
+      const Info2 = await page.evaluate(() => {
+        const resultArr = {};
+        const Address = document.querySelectorAll('table.table')[2];
+        const Info = Address.querySelectorAll("tbody tr");
+        for (let i = 1; i < Info.length; i++) {
+          const item = Info[i];
+          const ItemArr = item.querySelectorAll("td");
+          const key = ItemArr[0].innerText.replace(/\s/g, '');
+          const value = ItemArr[1].querySelector("strong").innerText;
+          resultArr[key] = value;
+        }
+        return resultArr;
+      })
+      const Info3 = await page.evaluate(() => {
+        const resultArr = {};
+        const Address = document.querySelectorAll('table.table')[3];
+        const Info = Address.querySelectorAll("tbody tr");
+        for (let i = 1; i < Info.length; i++) {
+          const item = Info[i];
+          const ItemArr = item.querySelectorAll("td");
+          const key = ItemArr[0].innerText.replace(/\s/g, '');
+          const value = ItemArr[1].querySelector("strong").innerText;
+          resultArr[key] = value;
+        }
+        return resultArr;
+      })
+
+      const Users = { ...User, ...Info, ...Info2, ...Info3 };
+      const post = new UserModel(Users);
+      post.save();
+      setTimeout(() => {
+        console.log("numberAll :", numberAll)
+        numberAll++;
+        GetAll();
+      }, 2000);
+    }
+    GetAll();
   } catch (err) {
     console.error(err);
     return null;
